b4c3c3db7dd6174dffd864b699637fbf32f0567b
jdomdev <jdomingodev@duck.com>
2024-11-04 23:30:23 +0100

    [CREATED] Expense Microservice

---
+++ expense/src/test/java/io/sunbit/app/expense/ExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sunbit.app.expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
