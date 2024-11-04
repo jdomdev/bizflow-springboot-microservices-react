08ccc0aa71a95818776871f65f911a1ce4b1e679
jdomdev <jdomingodev@duck.com>
2024-11-05 00:09:52 +0100

    [CONFIGURED] application.yml expense microservice

--- expense/src/main/java/io/sunbit/app/expense/ExpenseApplication.java
+++ expense/src/main/java/io/sunbit/app/expense/ExpenseApplication.java
@@ -2,7 +2,9 @@ package io.sunbit.app.expense;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ExpenseApplication {
 
