de3bf2516daf1ad2189b7f63c70e71cca7b50e39
jdomdev <jdomingodev@duck.com>
2024-11-05 12:37:21 +0100

    [CREATED] application.yml eureka microservice

--- expense/src/main/java/io/sunbit/app/expense/ExpenseApplication.java
+++ expense/src/main/java/io/sunbit/app/expense/ExpenseApplication.java
@@ -3,7 +3,7 @@ package io.sunbit.app.expense;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+// Explicit declaration as eureka customer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ExpenseApplication {
