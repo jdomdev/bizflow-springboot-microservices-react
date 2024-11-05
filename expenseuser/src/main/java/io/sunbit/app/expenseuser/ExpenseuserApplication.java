9d2541895759eace1f06ac0c7fa56d357888167d
jdomdev <jdomingodev@duck.com>
2024-11-05 18:52:04 +0100

    [CREATED] Employee and Expense User microservices

---
+++ expenseuser/src/main/java/io/sunbit/app/expenseuser/ExpenseuserApplication.java
@@ -0,0 +1,13 @@
+package io.sunbit.app.expenseuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseuserApplication.class, args);
+	}
+
+}
---
+++ expenseuser/src/test/java/io/sunbit/app/expenseuser/ExpenseuserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sunbit.app.expenseuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
